f99e9095a4f1dcdf08b2f2d21fd8b815eb6b626d
RVFET ‮　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　　‮ <58657896+RVFET@users.noreply.github.com>
2024-08-28 15:54:23 +0400

    v2

--- rich.go
+++ rich.go
@@ -5,109 +5,53 @@ package rich
 // Inspired by the rich library in Python.
 
 import (
-	"fmt"     // * Required for printing the result
-	"reflect" // * Required for detecting the type of the input
-	"regexp"  // * Required for parsing the tags
-	"strings" // * Required for string manipulation
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
 )
 
-// * Struct to define the color and style properties
-type color struct {
+type Style struct {
 	Name    string
 	Code    string
 	IsColor bool
 }
 
-// * List of colors and styles that can be applied to the text
-var Colors = []color{
-	{
-		Name:    "reset",
-		Code:    "0",
-		IsColor: false,
-	},
-	{
-		Name:    "unstyle",
-		Code:    "22",
-		IsColor: false,
-	},
-	{
-		Name:    "b",
-		Code:    "1",
-		IsColor: false,
-	},
-	{
-		Name:    "i",
-		Code:    "3",
-		IsColor: false,
-	},
-	{
-		Name:    "u",
-		Code:    "4",
-		IsColor: false,
-	},
-	{
-		Name:    "s",
-		Code:    "9",
-		IsColor: false,
-	},
-	{
-		Name:    "blink",
-		Code:    "5",
-		IsColor: false,
-	},
-	{
-		Name:    "x",
-		Code:    "7",
-		IsColor: false,
-	},
-	{
-		Name:    "white",
-		Code:    "97",
-		IsColor: true,
-	},
-	{
-		Name:    "gray",
-		Code:    "37",
-		IsColor: true,
-	},
-	{
-		Name:    "red",
-		Code:    "31",
-		IsColor: true,
-	},
-	{
-		Name:    "green",
-		Code:    "32",
-		IsColor: true,
-	},
-	{
-		Name:    "cyan",
-		Code:    "36",
-		IsColor: true,
-	},
-	{
-		Name:    "blue",
-		Code:    "34",
-		IsColor: true,
-	},
-	{
-		Name:    "yellow",
-		Code:    "33",
-		IsColor: true,
-	},
+var (
+	styles = []Style{
+		{"reset", "0", false},
+		{"unstyle", "22", false},
+		{"b", "1", false},
+		{"i", "3", false},
+		{"u", "4", false},
+		{"s", "9", false},
+		{"blink", "5", false},
+		{"x", "7", false},
+		{"white", "97", true},
+		{"gray", "37", true},
+		{"red", "31", true},
+		{"green", "32", true},
+		{"cyan", "36", true},
+		{"blue", "34", true},
+		{"yellow", "33", true},
+	}
+
+	styleMap = make(map[string]Style)
+)
+
+func init() {
+	for _, style := range styles {
+		styleMap[style.Name] = style
+	}
 }
 
-// & Helper function to get the color code by the color name
-func getColorByName(name string) string {
-	for _, c := range Colors {
-		if c.Name == name {
-			return "\033[" + c.Code + "m"
-		}
+func getStyle(name string) string {
+	if style, ok := styleMap[name]; ok {
+		return "\033[" + style.Code + "m"
 	}
 	return "\033[37m"
 }
 
-// & Helper function to parse the tags and forward them to `applyStyling`
 func parseTags(str string) string {
 	var stack []string
 	segments := strings.Split(str, "[")
@@ -122,48 +66,33 @@ func parseTags(str string) string {
 		}
 		tags, rest := parts[0], parts[1]
 
-		tagParts := strings.Fields(tags)
-		for _, tag := range tagParts {
+		for _, tag := range strings.Fields(tags) {
 			tag = strings.ToLower(strings.Trim(tag, "[]"))
 			if strings.HasPrefix(tag, "/") {
 				if len(stack) > 0 {
 					stack = stack[:len(stack)-1]
 				}
-			} else {
-				found := false
-				for _, c := range Colors {
-					if c.Name == tag {
-						stack = append(stack, c.Code)
-						found = true
-						break
-					}
-				}
-				if !found {
-					continue
-				}
+			} else if style, ok := styleMap[tag]; ok {
+				stack = append(stack, style.Code)
 			}
 		}
 
 		segments[i] = applyStyling(rest, stack)
 	}
 
-	styledStr := strings.Join(segments, "")
-	return styledStr
+	return strings.Join(segments, "")
 }
 
-// & Helper function to convert the curated styles to the ANSI escape codes
 func applyStyling(str string, stack []string) string {
-	styleCode := strings.Join(stack, ";")
-	return fmt.Sprintf("\033[%sm%s", styleCode, str)
+	return fmt.Sprintf("\033[%sm%s", strings.Join(stack, ";"), str)
 }
 
-// & Helper function to colorize some common keywords
 func colorizeKeywords(input string) string {
 	keywords := map[string]string{
-		"success": getColorByName("green"),
-		"error":   getColorByName("red"),
-		"warning": getColorByName("yellow"),
-		"info":    getColorByName("cyan"),
+		"success": getStyle("green"),
+		"error":   getStyle("red"),
+		"warning": getStyle("yellow"),
+		"info":    getStyle("cyan"),
 	}
 
 	for keyword, colorCode := range keywords {
@@ -176,70 +105,57 @@ func colorizeKeywords(input string) string {
 	return input
 }
 
-// & Helper function formats the input value based on its type.
 func formatValue(v reflect.Value) string {
-	var result strings.Builder
-	switch v.Kind() {
-	case reflect.String:
-		result.WriteString(formatString(v.String()))
+	switch reflect.TypeOf(v.Interface()).Kind() {
+	case reflect.Float32, reflect.Float64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return formatNumber(v)
 	case reflect.Map:
-		result.WriteString(formatMap(v))
+		return formatMap(v)
 	case reflect.Slice:
-		result.WriteString(formatSlice(v))
+		return formatSlice(v)
 	case reflect.Struct:
-		result.WriteString(formatStruct(v))
+		return formatStruct(v)
+	case reflect.Bool:
+		return formatBool(v)
 	default:
-		result.WriteString(parseTags(fmt.Sprintf("%v", v)))
+		return formatString(v.Interface())
 	}
-	return result.String()
 }
 
-// & Helper function formats a string value.
-func formatString(str string) string {
-	str = parseTags(str)
-	str = colorizeKeywords(str)
-	return str
+func formatString(str any) string {
+	return colorizeKeywords(parseTags(fmt.Sprintf("%v", str)))
 }
 
-// & Helper function formats a boolean value.
 func formatBool(v reflect.Value) string {
-	if v.Bool() {
-		return getColorByName("green") + "true" + getColorByName("white")
+	if reflect.ValueOf(v.Interface()).Bool() {
+		return parseTags("[green][bold]true[/]")
 	}
-	return getColorByName("red") + "false" + getColorByName("white")
+	return parseTags("[red][bold]false[/]")
 }
 
-// & Helper function formats a float or integer or hexadecimal value.
-func formatNumber(v reflect.Value) string {
-	return getColorByName("cyan") + parseTags(fmt.Sprintf("%v", v)) + getColorByName("white")
+func formatNumber(v any) string {
+	return parseTags(fmt.Sprintf("[cyan][bold]%v[/]", v))
 }
 
-// & Helper function formats a map value.
 func formatMap(v reflect.Value) string {
 	var result strings.Builder
 	result.WriteString("{\n")
 	for _, key := range v.MapKeys() {
-		result.WriteString(" " + parseTags(fmt.Sprintf("[gray]%s[/gray]: ", key.String())))
-		result.WriteString(formatValue(v.MapIndex(key)) + "\n")
+		left_side := formatValue(key)
+		right_side := formatValue(v.MapIndex(key))
+
+		result.WriteString(fmt.Sprintf("  \"%s\": %s,\n", left_side, right_side))
 	}
 	result.WriteString("}")
 	return result.String()
 }
 
-// & Helper function formats a slice value.
 func formatSlice(v reflect.Value) string {
 	var result strings.Builder
 	result.WriteString("[ ")
 	for i := 0; i < v.Len(); i++ {
 		element := v.Index(i)
-		switch value := element.Interface().(type) {
-		case int, int8, int16, int32, int64:
-			result.WriteString(getColorByName("cyan") + fmt.Sprint(value) + getColorByName("white"))
-		case string:
-			result.WriteString(getColorByName("gray") + "\"" + value + "\"" + getColorByName("white"))
-		default:
-			result.WriteString(getColorByName("gray") + "\"" + fmt.Sprint(value) + "\"" + getColorByName("white"))
-		}
+		result.WriteString(formatValue(element))
 		if i < v.Len()-1 {
 			result.WriteString(", ")
 		}
@@ -248,77 +164,68 @@ func formatSlice(v reflect.Value) string {
 	return result.String()
 }
 
-// & Helper function formats a struct value.
 func formatStruct(v reflect.Value) string {
 	var result strings.Builder
 	result.WriteString("{\n")
 	for i := 0; i < v.NumField(); i++ {
-		result.WriteString(" " + parseTags(fmt.Sprintf("%s: ", v.Type().Field(i).Name)))
-		result.WriteString(formatValue(v.Field(i)) + "\n")
+		result.WriteString(fmt.Sprintf(" %s: %s\n",
+			parseTags(v.Type().Field(i).Name),
+			formatValue(v.Field(i))))
 	}
 	result.WriteString("}")
 	return result.String()
 }
 
-// * Main function that's been exposed to other packages to print the formatted value
-func Print(args ...interface{}) {
+func Print(args ...any) {
 	var formattedStrings []string
 
 	for _, arg := range args {
-		// Determine the type of the argument
-		argType := reflect.TypeOf(arg)
-
-		// Format the argument based on its type
+		v := reflect.ValueOf(arg)
 		var formattedArg string
-		switch argType.Kind() {
+
+		switch v.Kind() {
 		case reflect.String:
-			formattedArg = formatString(arg.(string))
+			formattedArg = formatString(v.String())
 		case reflect.Bool:
-			formattedArg = formatBool(reflect.ValueOf(arg))
+			formattedArg = formatBool(v)
 		case reflect.Float32, reflect.Float64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			formattedArg = formatNumber(reflect.ValueOf(arg))
+			formattedArg = formatNumber(v)
 		case reflect.Map:
-			formattedArg = formatMap(reflect.ValueOf(arg))
+			formattedArg = formatMap(v)
 		case reflect.Slice:
-			formattedArg = formatSlice(reflect.ValueOf(arg))
+			formattedArg = formatSlice(v)
 		case reflect.Struct:
-			formattedArg = formatStruct(reflect.ValueOf(arg))
+			formattedArg = formatStruct(v)
 		default:
 			formattedArg = parseTags(fmt.Sprintf("%v", arg))
 		}
 
-		// Append the formatted argument to the slice
 		formattedStrings = append(formattedStrings, formattedArg)
 	}
 
-	// Concatenate all formatted strings into a single string
-	formattedOutput := strings.Join(formattedStrings, " ")
-
-	// Print the concatenated string
-	fmt.Println(formattedOutput)
+	fmt.Println(strings.Join(formattedStrings, " "))
 }
 
-// * Info - Print the input with an abbreviation and blue color using Print
-func Info(input interface{}) {
-	Print("[blue][b]INFO:[/b][/blue] " + input.(string))
+func logWithPrefix(prefix string, args ...any) {
+	Print(append([]any{prefix}, args...)...)
 }
 
-// * Success - Print the input with an abbreviation and green color using Print
-func Success(input interface{}) {
-	Print("[green][b]SUCC:[/b][/green] " + input.(string))
+func Info(args ...any) {
+	logWithPrefix("[blue][b]INFO:[/b][/blue]", args...)
 }
 
-// * Error - Print the input with an abbreviation and red color using Print
-func Error(input interface{}) {
-	Print("[red][b]ERRR:[/b][/red] " + input.(string))
+func Success(args ...any) {
+	logWithPrefix("[green][b]SUCC:[/b][/green]", args...)
 }
 
-// * Warning - Print the input with an abbreviation and yellow color using Print
-func Warning(input interface{}) {
-	Print("[yellow][b]WARN:[/b][/yellow] " + input.(string))
+func Error(args ...any) {
+	logWithPrefix("[red][b]ERRR:[/b][/red]", args...)
 }
 
-// * Debug - Print the input with an abbreviation and gray color using Print
-func Debug(input interface{}) {
-	Print("[gray][b]DEBUG:[/b][/gray] " + input.(string))
+func Warning(args ...any) {
+	logWithPrefix("[yellow][b]WARN:[/b][/yellow]", args...)
+}
+
+func Debug(args ...any) {
+	logWithPrefix("[gray][b]DEBUG:[/b][/gray]", args...)
 }
